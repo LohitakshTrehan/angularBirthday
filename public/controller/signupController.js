8f6df2f3ee49f53480054d416ad1475368845283
Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>
2019-09-30 20:10:32 +0530

    Basic UI validation of signup almost completed, to be done: adding in local storage, current user, routing to home, isValid and isNotValid classes to be added
    Signed-off-by: Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>

--- public/controller/signupController.js
+++ public/controller/signupController.js
@@ -3,7 +3,7 @@ myApp.controller("signupController",['$scope',function($scope){
     document.getElementById("RegisterPerson").addEventListener('click',function(event){
         event.preventDefault();
         //start registering the person and look for inline error marking
-        let name_value = $scope.Name.trim();
+        let name_value = $scope.Name;
         let dob = $scope.BirthDate;
         let empId = $scope.empId;
         let enteredEmail = $scope.Email;
@@ -13,131 +13,134 @@ myApp.controller("signupController",['$scope',function($scope){
             dob:false,
             empId:false,
             email:false,
+            email_exist:false,
             pass:false,
             cpass:false
         }
-        var isValid = validate(name_value,dob,empId,enteredEmail,pass);
+        var output = (validate(name_value,dob,empId,enteredEmail,pass,$scope));
+        $scope = output.scope; 
+        $scope.$apply();
     });
 }])
 
-function validate(name_value,dob,empId,enteredEmail,pass){
+function validate(name_value,dob,empId,enteredEmail,pass,scope){
 
     //NO VALIDATION TEST FOR 29, 30, 31 FEB OR ANY OTHER INVALID DATE FOR A MONTH OR AN IMPROPER MONTH ARE CODED, IN CASE ERROR, DEFAULT DATE = 1, DEFAULT MONTH = 1
 
     var letters = /^[A-Za-z]+$/;
     var email = /^[A-Za-z0-9]+(?:[\.][A-Za-z0-9]+)*$/
     var empIdPattern = /^[0-9]+$/;
-    var nameArr = name_value.split(" ");
-    var confirm_pass = $scope.confirm_pass;
+    var nameArr = undefined;
+    if(name_value!==undefined){
+        name_value = name_value.trim();
+        nameArr = name_value.split(" ");
+    }
+    var confirm_pass = scope.confirm_pass;
     var isValid = false;
     var err = 0;
     //chnage using join
-    for(let i=0;i<nameArr.length;i++){
-        if(nameArr[i].match(letters)===null){
-            document.getElementById("wrap_name").setAttribute("class","mb-1");
-            $scope.show_error.name = true;
-            //document.getElementById("err_name").style["display"]="inline";
-            isValid = false;
-            err = 1;
-            break;
+    if(nameArr !== undefined){
+        for(let i=0;i<nameArr.length;i++){
+            if(nameArr[i].match(letters)===null){
+                document.getElementById("wrap_name").setAttribute("class","mb-1");
+                scope.show_error.name = true;
+                //document.getElementById("err_name").style["display"]="inline";
+                isValid = false;
+                err = 1;
+                break;
+            }
+        }
+        if(err===0){
+            document.getElementById("wrap_name").setAttribute("class","mb-3");
+            scope.show_error.name = false;
+            //document.getElementById("err_name").style["display"]="none";
         }
     }
-
-    if(err===0){
-        document.getElementById("wrap_name").setAttribute("class","mb-3");
-        $scope.show_error.name = false;
-        //document.getElementById("err_name").style["display"]="none";
+    else{
+        scope.show_error.name = true;
+        isValid = false;
     }
+
     var isBorn = seeIfBorn(dob);
-    if( !isBorn || empId.match(empIdPattern)===null){
+    if( !isBorn || empId===undefined || empId.match(empIdPattern)===null){
         document.getElementById("wrap_input").setAttribute("class","mb-1 d-flex flex-row");
         isValid = false;
         if(!isBorn){
-            dob_node.classList.remove("is-valid")
-            dob_node.classList.add("is-invalid")
-            document.getElementById("err_birth").style["display"]="inline";
+            scope.show_error.dob = true;
+            //document.getElementById("err_birth").style["display"]="inline";
         }
-        if(empId.match(empIdPattern)===null){
-            empId_node.classList.remove("is-valid")
-            empId_node.classList.add("is-invalid")
-            document.getElementById("err_empId").style["display"]="inline";
+        if(empId===undefined || empId.match(empIdPattern)===null){
+            scope.show_error.empId = true;
+            //document.getElementById("err_empId").style["display"]="inline";
         }
     }
-    if(isBorn && empId.match(empIdPattern)!==null){
+    if(isBorn && empId!==undefined && empId.match(empIdPattern)!==null){
         document.getElementById("wrap_input").setAttribute("class","mb-3 d-flex flex-row");
     }
     if(isBorn){
-        dob_node.classList.remove("is-invalid")
-        dob_node.classList.add("is-valid")
-        document.getElementById("err_birth").style["display"]="none";
+        scope.show_error.dob = false;
+        //document.getElementById("err_birth").style["display"]="none";
     }
-    if(empId.match(empIdPattern)!==null){
-        empId_node.classList.remove("is-invalid")
-        empId_node.classList.add("is-valid")
-        document.getElementById("err_empId").style["display"]="none";
+    if(empId!==undefined && empId.match(empIdPattern)!==null){
+        scope.show_error.empId = false;
+        //document.getElementById("err_empId").style["display"]="none";
     }
     
-    if(enteredEmail.match(email)===null){
+    if(enteredEmail===undefined || enteredEmail.match(email)===null){
         document.getElementById("email-input-wrapper").setAttribute("class","mb-1");
-        email_node.classList.remove("is-valid")
-        email_node.classList.add("is-invalid")
-        document.getElementById("err_email").style["display"]="inline";
+        scope.show_error.email = true;
+        //document.getElementById("err_email").style["display"]="inline";
         isValid = false;
     }
     else{
         document.getElementById("email-input-wrapper").setAttribute("class","mb-3");
-        email_node.classList.remove("is-invalid")
-        email_node.classList.add("is-valid")
-        document.getElementById("err_email").style["display"]="none";
+        scope.show_error.email = false;
+        //document.getElementById("err_email").style["display"]="none";
     }
-
-    if(pass.length<5){
-        document.getElementById("wrap_pass").setAttribute("class","mb-1 d-flex flex-row");
+    if(enteredEmail!==undefined && emailExists(enteredEmail)){
+        document.getElementById("email-input-wrapper").setAttribute("class","mb-1");
+        scope.show_error.email_exist = true;
+        //document.getElementById("err_email_exists").style["display"]="inline";
         isValid = false;
-        pass_node.classList.remove("is-valid")
-        pass_node.classList.add("is-invalid")
-        document.getElementById("err_pass").style["display"]="inline";
-
-        confirm_pass_node.classList.remove("is-valid")
-        confirm_pass_node.classList.remove("is-invalid")
-        document.getElementById("err_cpass").style["display"]="none";
     }
     else{
-        pass_node.classList.remove("is-invalid")
-        pass_node.classList.add("is-valid")
-        document.getElementById("err_pass").style["display"]="none";
-        if(pass!==confirm_pass){
+        document.getElementById("email-input-wrapper").setAttribute("class","mb-3");
+        scope.show_error.email_exist = false;
+        //document.getElementById("err_email_exists").style["display"]="none";
+    }
+
+    if(pass===undefined || pass.length<5){
+        document.getElementById("wrap_pass").setAttribute("class","mb-1 d-flex flex-row");
+        isValid = false;
+        scope.show_error.pass = true;
+        //document.getElementById("err_pass").style["display"]="inline";
+
+        scope.show_error.cpass = false;
+        //document.getElementById("err_cpass").style["display"]="none";
+    }
+    else{
+        scope.show_error.pass = false;
+        //document.getElementById("err_pass").style["display"]="none";
+        if(confirm_pass===undefined || pass!==confirm_pass){
             document.getElementById("wrap_pass").setAttribute("class","mb-1 d-flex flex-row");
-            confirm_pass_node.classList.remove("is-valid")
-            confirm_pass_node.classList.add("is-invalid")
-            document.getElementById("err_cpass").style["display"]="inline";
+            scope.show_error.cpass = true;
+            //document.getElementById("err_cpass").style["display"]="inline";
             isValid = false;
         }
         else{
             document.getElementById("wrap_pass").setAttribute("class","mb-3 d-flex flex-row");
-            confirm_pass_node.classList.remove("is-invalid")
-            confirm_pass_node.classList.add("is-valid")
-            document.getElementById("err_cpass").style["display"]="none";
+            scope.show_error.cpass = false;
+            //document.getElementById("err_cpass").style["display"]="none";
         }
     }
-    if(emailExists(enteredEmail)){
-        document.getElementById("email-input-wrapper").setAttribute("class","mb-1");
-        email_node.classList.remove("is-valid")
-        email_node.classList.add("is-invalid")
-        document.getElementById("err_email_exists").style["display"]="inline";
-        isValid = false;
+    return {
+        isValid: isValid,
+        scope:scope
     }
-    else{
-        document.getElementById("email-input-wrapper").setAttribute("class","mb-3");
-        email_node.classList.remove("is-invalid")
-        email_node.classList.add("is-valid")
-        document.getElementById("err_email_exists").style["display"]="none";
-    }
-    return isValid;
 }
 
 function seeIfBorn(bornOn){
-    if(bornOn==="")
+    if(bornOn==="" || bornOn === undefined)
         return false;
     var today = new Date();
     var getYear = today.getFullYear();
